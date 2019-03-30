ba28f579d90943e976373ceb8716fc2d2cf4df92
shilk <shilk.e82@gmail.com>
2019-03-30 21:52:50 +0300

    Player, InteractableObject classes

--- Project/Assets/Scripts/PlayerInput/InteractableObject.cs
+++ Project/Assets/Scripts/PlayerInput/InteractableObject.cs
@@ -1,15 +1,33 @@
+using System;
+using Dynamic_objects;
 using UnityEngine;
 using UnityEngine.Networking;
 
 namespace PlayerInput
 {
-    public class InteractableObject : MonoBehaviour
+    public class InteractableObject : NetworkBehaviour
     {
-        [SerializeField] private GameObject activationObject;
+        [SerializeField] private GameObject activationObjectParent;
+        private IActivateObject activateObject;
         
-        public void Interact(Player player)
+        public void Interact(GameObject player, bool activated)
         {
             
+        }
+
+        [ClientRpc]
+        private void RpcInteract(GameObject playerObject, bool activate)
+        {
+            if (activateObject == null)
+                activateObject = activationObjectParent.GetComponent<IActivateObject>();
+            if (activateObject == null)
+                throw new NullReferenceException("Connected GameObject doesnt have activateable object!");
+            
+            Player player = null;
+            if (playerObject != null)
+                player = playerObject.GetComponent<Player>();
+            
+            activateObject.Activate(player, activate);
         }
     }
 }
--- Project/Assets/Scripts/PlayerInput/Player.cs
+++ Project/Assets/Scripts/PlayerInput/Player.cs
@@ -1,9 +1,95 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace PlayerInput
 {
-    public class Player : MonoBehaviour
+    public class Player : NetworkBehaviour
     {
+        [SerializeField] private Camera camera;
         
+        
+        [SyncVar(hook = nameof(ApplyColorChange))] private Color playerColor;
+
+        private InteractableObject prevObject = null;
+        private List<Material> materials = new List<Material>();
+        
+        public Color PlayerColor
+        {
+            get { return playerColor; }
+        }
+
+        private void Awake()
+        {
+            playerColor = Random.ColorHSV();
+           
+        }
+
+        private void ApplyColorChange(Color newPlayerColor)
+        {
+            foreach (var renderer in GetComponentsInChildren<Renderer>())
+            {
+                foreach (var material in renderer.materials)
+                {
+                    material.color = newPlayerColor;
+                }
+            }
+        }
+
+        private void Update()
+        {
+            CheckForPlayerInput();
+        }
+
+        [ClientCallback]
+        private void CheckForPlayerInput()
+        {
+            if (!isLocalPlayer)
+                return;
+
+            if (Input.GetButton("Fire1"))
+            {
+                RaycastHit hit;
+                var ray = camera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit, 1000f))
+                {
+                    var interactedObject = hit.transform.GetComponent<InteractableObject>();
+                    if(prevObject && prevObject != interactedObject)
+                        CmdSetInteraction(prevObject.gameObject, false);
+                    if (interactedObject && interactedObject != prevObject)
+                    {
+                        prevObject = interactedObject;
+                        CmdSetInteraction(interactedObject.gameObject, true);
+                    }
+                    
+                }
+                else if (prevObject)
+                {
+                    CmdSetInteraction(prevObject.gameObject, false);
+                    prevObject = null;
+                }
+            }
+        }
+
+        [Command]
+        private void CmdSetInteraction(GameObject interactionObject, bool activated)
+        {
+            var interaction = interactionObject.GetComponent<InteractableObject>();
+            interaction.Interact(gameObject, activated);
+        }
+
+        [Command]
+        public void CmdChangePlayerColor(Color newColor)
+        {
+            playerColor = newColor;
+        }
+        
+        #if UNITY_EDITOR
+        [ContextMenu("Test color change")]
+        private void TestColorChange()
+        {
+            CmdChangePlayerColor(Random.ColorHSV());
+        }
+        #endif
     }
 }
